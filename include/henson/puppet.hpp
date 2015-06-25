e3a00998c46fc5040c56d3e87f4db4efe8bd6ba2
Dmitriy Morozov <dmitriy@mrzv.org>
2015-06-25 13:18:27 -0700

    Move Puppet out into i/h/puppet.hpp

---
+++ include/henson/puppet.hpp
@@ -0,0 +1,74 @@
+#ifndef HENSON_PUPPET_HPP
+#define HENSON_PUPPET_HPP
+
+#include <vector>
+#include <stdexcept>
+
+#include <unistd.h>
+#include <errno.h>
+#include <dlfcn.h>
+
+#include <mpi.h>
+#include <boost/context/all.hpp>
+
+#include <format.h>
+#include <henson/data.hpp>
+
+namespace henson
+{
+namespace bc = boost::context;
+
+struct Puppet
+{
+    typedef             int  (*MainType)(int argc, char *argv[]);
+    typedef             void (*SetContextType)(void* parent, void* local);
+    typedef             void (*SetWorldType)(MPI_Comm world);
+    typedef             void (*SetNameMapType)(void* namemap);
+
+                        Puppet(const std::string& fn, int argc, char** argv, MPI_Comm world, henson::NameMap* namemap):
+                            filename_(fn),
+                            argc_(argc), argv_(argv),
+                            //stack_(bc::stack_traits::default_size())      // requires Boost 1.58
+                            stack_(1024*1024)
+                        {
+                            void* lib = dlopen(fn.c_str(), RTLD_LAZY);
+
+                            main_ = get_function<MainType>(lib, "main");
+
+                            get_function<SetContextType>(lib, "henson_set_contexts")(&from_, &to_);
+                            get_function<SetWorldType>  (lib, "henson_set_world")(world);
+                            get_function<SetNameMapType>(lib, "henson_set_namemap")(namemap);
+
+                            to_ = bc::make_fcontext(&stack_[0] + stack_.size(), stack_.size(), exec);
+                        }
+
+    void                proceed()               { bc::jump_fcontext(&from_, to_, (intptr_t) this); }
+    void                yield()                 { bc::jump_fcontext(&to_, from_, 0); }
+
+    bool                running() const         { return running_; }
+
+    static void         exec(intptr_t self_)    { Puppet* self = (Puppet*) self_; self->running_ = true; self->main_(self->argc_,self->argv_); self->running_ = false; self->yield(); }
+
+    template<class T>
+    T                   get_function(void* lib, const char* name)
+    {
+        T f = (T) dlsym(lib, name);
+        if (f == NULL)
+            throw std::runtime_error(fmt::format("Could not load {}() in {}\n{}\n", name, filename_, dlerror()));
+        return f;
+    }
+
+    std::string         filename_;
+    int                 argc_;
+    char**              argv_;
+    std::vector<char>   stack_;
+
+    MainType            main_;
+    bc::fcontext_t      from_, to_;
+    bool                running_;
+};
+
+
+}
+
+#endif
