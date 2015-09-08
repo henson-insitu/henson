b9c53caf0970036b1f6eb3e990172d05248778c9
Dmitriy Morozov <dmitriy@mrzv.org>
2015-09-08 09:47:47 -0700

    tools/timer.cpp optionally make decisions only on root (and broadcasts to the rest)

--- tools/timer.cpp
+++ tools/timer.cpp
@@ -49,10 +49,20 @@ size_t      get_henson_timer(std::string name)
 
 int main(int argc, char** argv)
 {
+    if (!henson_active())
+    {
+        fmt::print("{} should run under henson\n", argv[0]);
+        return 1;
+    }
+    MPI_Comm world = henson_get_world();
+    int rank;
+    MPI_Comm_rank(world, &rank);
+
     using namespace opts;
     Options ops(argc, argv);
 
     bool increment = ops >> Present('i', "increment", "increment the timer value");
+    bool root      = ops >> Present('r', "root",      "perform comparison on the root node only (and broadcast decision to the rest)");
 
     std::string cmd;
     ops >> PosOption(cmd);
@@ -91,15 +101,25 @@ int main(int argc, char** argv)
         size_t t1 = get_henson_timer(name1),
                t2 = get_henson_timer(name2);
 
-        if (cmd == "<" && t1 < t2)
-            return 0;
-        else if (cmd == "<=" && t1 <= t2)
-            return 0;
-        else if (cmd == ">"  && t1 >  t2)
-            return 0;
-        else if (cmd == ">=" && t1 >= t2)
-            return 0;
-        else
-            return 1;
+        int res;
+        if (!root || rank == 0)
+        {
+            if (cmd == "<" && t1 < t2)
+                res = 0;
+            else if (cmd == "<=" && t1 <= t2)
+                res = 0;
+            else if (cmd == ">"  && t1 >  t2)
+                res = 0;
+            else if (cmd == ">=" && t1 >= t2)
+                res = 0;
+            else
+                res = 1;
+
+            if (root)
+                MPI_Bcast(&res, 1, MPI_INT, 0, world);
+        } else
+            MPI_Bcast(&res, 1, MPI_INT, 0, world);
+
+        return res;
     }
 }
