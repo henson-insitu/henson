9c971afb0de3830473f6a6f76b31e7c307e390ac
Dmitriy Morozov <dmitriy@mrzv.org>
2015-09-14 10:44:39 -0700

    Move get_time() from tools/timer.cpp to i/h/timer.hpp

--- tools/timer.cpp
+++ tools/timer.cpp
@@ -1,38 +1,15 @@
 #include <string>
 
-#include <sys/time.h>
-
-#ifdef __MACH__
-#include <mach/clock.h>
-#include <mach/mach.h>
-#endif
-
 #include <mpi.h>
 
 #include <henson/data.h>
 #include <henson/data.hpp>
 #include <henson/context.h>
 
+#include <henson/time.hpp>
+
 #include <format.h>
 #include <opts/opts.h>
-
-typedef             unsigned long                       time_type;
-
-time_type
-get_time()
-{
-#ifdef __MACH__ // OS X does not have clock_gettime, use clock_get_time
-    clock_serv_t cclock;
-    mach_timespec_t ts;
-    host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
-    clock_get_time(cclock, &ts);
-    mach_port_deallocate(mach_task_self(), cclock);
-#else
-    timespec ts;
-    clock_gettime(CLOCK_REALTIME, &ts);
-#endif
-    return ts.tv_sec*1000 + ts.tv_nsec/1000000;
-}
 
 std::string timer(std::string name)                 { return "timer_" + name; }
 std::string timer_start(std::string name)           { return "timer_start_" + name; }
@@ -71,7 +48,7 @@ int main(int argc, char** argv)
         std::string name;
         ops >> PosOption(name);
 
-        henson_save_size_t(timer_start(name).c_str(), get_time());
+        henson_save_size_t(timer_start(name).c_str(), henson::get_time());
     } else if (cmd == "stop")
     {
         std::string name;
@@ -79,7 +56,7 @@ int main(int argc, char** argv)
 
         size_t start;
         henson_load_size_t(timer_start(name).c_str(), &start);
-        size_t elapsed = get_time() - start;
+        size_t elapsed = henson::get_time() - start;
 
         if (!increment)
             henson_save_size_t(timer(name).c_str(), elapsed);
