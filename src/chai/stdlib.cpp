70051f35cda0e1385e27a600eac1b3221816bdeb
Dmitriy Morozov <dmitriy@mrzv.org>
2021-01-14 20:15:26 -0800

    Add readlines() to chai + examples/utils

--- src/chai/stdlib.cpp
+++ src/chai/stdlib.cpp
@@ -1,7 +1,26 @@
+#include <vector>
+#include <string>
+#include <fstream>
+
 #include <henson/chai/stdlib.hpp>
 #include <chaiscript/chaiscript_stdlib.hpp>
 
 std::unique_ptr<chaiscript::ChaiScript> chai_stdlib()
 {
-    return std::unique_ptr<chaiscript::ChaiScript> { new chaiscript::ChaiScript(chaiscript::Std_Lib::library()) };
+    std::unique_ptr<chaiscript::ChaiScript> chai { new chaiscript::ChaiScript(chaiscript::Std_Lib::library()) };
+
+    using StringVector = std::vector<std::string>;
+    chai->add(chaiscript::bootstrap::standard_library::vector_type<StringVector>("StringVector"));
+    chai->add(chaiscript::fun([](std::string filename)
+                             {
+                                std::vector<std::string> lines;
+                                std::ifstream in(filename);
+                                std::string line;
+                                while (std::getline(in, line))
+                                    lines.push_back(line);
+
+                                return lines;
+                             }),               "readlines");
+
+    return chai;
 }
