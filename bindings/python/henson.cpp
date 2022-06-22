032c97208537d903a3912e4ed419296f27295f93
Dmitriy Morozov <dmitriy@mrzv.org>
2022-06-22 09:50:34 -0700

    Minor cleanup (move capsule code into a separate header)

--- bindings/python/henson.cpp
+++ bindings/python/henson.cpp
@@ -1,6 +1,5 @@
 #include <vector>
 #include <string>
-#include <type_traits>
 
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
@@ -14,43 +13,10 @@ namespace py = pybind11;
 #include <mpi.h>
 
 #if defined(HENSON_MPI4PY)
-#include "mpi_comm.h"
+#include "mpi-comm.h"
 #endif
 
-
-// MPI_Comm is a pointer in OpenMPI
-template<class Comm, typename std::enable_if<std::is_pointer<Comm>::value,bool>::type = true>
-py::capsule to_capsule(Comm comm)
-{
-    return py::capsule(comm);
-}
-
-template<class Comm>
-typename std::enable_if<std::is_pointer<Comm>::value, Comm>::type
-from_capsule(py::capsule c)
-{
-    return c;
-}
-
-// MPI_Comm is an integer in MPICH
-template<class Comm, typename std::enable_if<std::is_integral<Comm>::value,bool>::type = true>
-py::capsule to_capsule(Comm comm)
-{
-    intptr_t comm_ = static_cast<intptr_t>(comm);
-    void* comm__ = reinterpret_cast<void*>(comm_);
-    return py::capsule(comm__);
-}
-
-template<class Comm>
-typename std::enable_if<std::is_integral<Comm>::value, Comm>::type
-from_capsule(py::capsule c)
-{
-    void* comm_ = c;
-    intptr_t comm__ = reinterpret_cast<intptr_t>(comm_);
-    Comm comm = static_cast<Comm>(comm__);
-    return comm;
-}
-
+#include "mpi-capsule.h"
 
 PYBIND11_MODULE(pyhenson, m)
 {
